b49ff8e7c5fc7a5fe33c10f2c89ffa7e74460566
Benjamin Elwyn <ben@elwyn.co>
2021-07-27 14:29:23 +0100

    improve typescript rules

--- typescript.js
+++ typescript.js
@@ -1,17 +1,19 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
+  parser: '@typescript-eslint/parser',
 
-	plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
 
-	extends: ['plugin:@typescript-eslint/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended'],
 
-	rules: {
-		// Use TS for these
-		'react/prop-types': 'off',
-		'react/require-default-props': 'off',
-		// TS rules
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-	},
+  rules: {
+    // Use TS for these
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    // TS rules
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
 }
